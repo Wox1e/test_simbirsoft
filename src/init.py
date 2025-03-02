2991fc1ada3f14803039fd985b1096c3eca4d713
woxie <woxie@mail.ru>
2025-03-02 13:53:54 +0400

    structure refactoring

--- src/init.py
+++ src/init.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path="drivers/chromedriver.exe")
+service = Service(executable_path="../drivers/chromedriver.exe")
 driver = webdriver.Chrome(service = service)
 driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["https://pagead2.googlesyndication.com"]})           # Blocking Google AdSense unaccessible url
 driver.execute_cdp_cmd('Network.enable', {})
